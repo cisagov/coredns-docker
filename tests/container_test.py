4418c23c8c96f87d043501124001f85c2084d2f1
Kali <dpmurphy@wpi.edu>
2022-06-23 09:49:35 -0400

    fixing python syntax

--- tests/container_test.py
+++ tests/container_test.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env pytest -vs
-"""Tests for example container."""
+#!/usr/bin/env pytest
+#Tests for example container.
 
 # Standard Python Libraries
 import os
@@ -19,7 +19,7 @@ VERSION_FILE = "src/version.txt"
 
 
 def test_container_count(dockerc):
-    """Verify the test composition and container."""
+#    """Verify the test composition and container."""
     # stopped parameter allows non-running containers in results
     assert (
         len(dockerc.containers(stopped=True)) == 2
@@ -27,43 +27,39 @@ def test_container_count(dockerc):
 
 
 #TODO: Implement this test.
-"""
-def test_wait_for_ready(main_container):
-    """Wait for container to be ready."""
-    TIMEOUT = 10
-    for i in range(TIMEOUT):
-        if READY_MESSAGE in main_container.logs().decode("utf-8"):
-            break
-        time.sleep(1)
-    else:
-        raise Exception(
-            f"Container does not seem ready.  "
-            f'Expected "{READY_MESSAGE}" in the log within {TIMEOUT} seconds.'
-        )
-"""
+#def test_wait_for_ready(main_container):
+#    """Wait for container to be ready."""
+#    TIMEOUT = 10
+#    for i in range(TIMEOUT):
+#        if READY_MESSAGE in main_container.logs().decode("utf-8"):
+#            break
+#        time.sleep(1)
+#    else:
+#        raise Exception(
+#            f"Container does not seem ready.  "
+#            f'Expected "{READY_MESSAGE}" in the log within {TIMEOUT} seconds.'
+#        )
 
 def test_wait_for_exits(main_container, version_container):
-    """Wait for containers to exit."""
+    #Wait for containers to exit.
     assert main_container.wait() == 0, "Container service (main) did not exit cleanly"
     assert (
         version_container.wait() == 0
     ), "Container service (version) did not exit cleanly"
 
 #TODO: Implement this test.
-
-"""
-def test_output(main_container):
-    """Verify the container had the correct output."""
-    main_container.wait()  # make sure container exited if running test isolated
-    log_output = main_container.logs().decode("utf-8")
-    assert SECRET_QUOTE in log_output, "Secret not found in log output."
-"""
+#def test_output(main_container):
+#    """Verify the container had the correct output."""
+#    main_container.wait()  # make sure container exited if running test isolated
+#    log_output = main_container.logs().decode("utf-8")
+#    assert SECRET_QUOTE in log_output, "Secret not found in log output."
+#
 
 @pytest.mark.skipif(
     RELEASE_TAG in [None, ""], reason="this is not a release (RELEASE_TAG not set)"
 )
 def test_release_version():
-    """Verify that release tag version agrees with the module version."""
+    #Verify that release tag version agrees with the module version.
     pkg_vars = {}
     with open(VERSION_FILE) as f:
         exec(f.read(), pkg_vars)  # nosec
@@ -74,7 +70,7 @@ def test_release_version():
 
 
 def test_log_version(version_container):
-    """Verify the container outputs the correct version to the logs."""
+    #Verify the container outputs the correct version to the logs.
     version_container.wait()  # make sure container exited if running test isolated
     log_output = version_container.logs().decode("utf-8").strip()
     pkg_vars = {}
@@ -87,7 +83,7 @@ def test_log_version(version_container):
 
 
 def test_container_version_label_matches(version_container):
-    """Verify the container version label is the correct version."""
+    #Verify the container version label is the correct version.
     pkg_vars = {}
     with open(VERSION_FILE) as f:
         exec(f.read(), pkg_vars)  # nosec
